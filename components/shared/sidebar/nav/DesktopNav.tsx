8865c5d4bee208bab7d5a274dd8ae01492efbc0a
Expak12 <aarongt1808@gmail.com>
2024-07-23 16:30:44 -0600

    "Updated dependencies and added new ones, including @radix-ui/react-dropdown-menu and next-themes, and modified various files in the app and components directories." "Also added dark mode changing and some fallbacks for friends and conversations pages"

--- components/shared/sidebar/nav/DesktopNav.tsx
+++ components/shared/sidebar/nav/DesktopNav.tsx
@@ -3,6 +3,7 @@
 
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
+import { ThemeToggle } from '@/components/ui/theme/theme-toggle';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import { useNavigation } from '@/hooks/useNavigation'
 import { UserButton } from '@clerk/nextjs';
@@ -37,6 +38,7 @@ const DesktopNav = () => {
             }</ul>
         </nav>
         <div className='flex flex-col items-center gap-4'>
+            <ThemeToggle />
             <UserButton />
         </div>
     </Card>
